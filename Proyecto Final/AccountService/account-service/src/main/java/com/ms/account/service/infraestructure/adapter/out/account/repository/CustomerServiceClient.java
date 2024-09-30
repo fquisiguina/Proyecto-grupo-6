18ccb26c134b75d14843116b2c94f9ab49a2fe37
Flavio Quisiguina <fquisiguina@est.ups.edu.ec>
2024-09-30 17:10:51 -0500

    cambios para funcionamiento de gateway

--- Proyecto Final/AccountService/account-service/src/main/java/com/ms/account/service/infraestructure/adapter/out/account/repository/CustomerServiceClient.java
+++ Proyecto Final/AccountService/account-service/src/main/java/com/ms/account/service/infraestructure/adapter/out/account/repository/CustomerServiceClient.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(url = "http://localhost:8084/api/v1", name = "customerServiceClient")
-//@FeignClient(name = "ACCOUNT-SERVICE")
+//@FeignClient(url = "http://localhost:8084/api/v1", name = "customerServiceClient")
+@FeignClient(name = "CUSTOMER-SERVICE")
 public interface CustomerServiceClient {
 
-    @GetMapping("/customers/{id}")
-    //@GetMapping("/api/v1/customers/{id}")
+    //@GetMapping("/customers/{id}")
+    @GetMapping("/api/v1/customers/{id}")
     ResponseEntity<Customer> getCustomerById(@PathVariable("id") Long id,
                                             @RequestHeader(value = "x-cm-client-request-id", required = true) String xCwClientRequestId,
                                             @RequestHeader(value = "x-cm-client-user-agent", required = true) String xCmClientUserAgent);
